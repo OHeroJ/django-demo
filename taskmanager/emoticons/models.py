40d719d250616d198d1246c473eec5c43483a3d2
laijihua <1164258202@qq.com>
2024-05-11 22:49:20 +0800

    Feat: 修改模型

--- taskmanager/emoticons/models.py
+++ taskmanager/emoticons/models.py
@@ -48,20 +48,20 @@ class PTag(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self) -> str:
-        return self.name + "("+ self.mode.name + ")"
+        return self.name
     
     class Meta:
         verbose_name = "图片标签"
         verbose_name_plural = "图片标签"
     
 class Pic(models.Model):
-    keywords = models.CharField(max_length=100, verbose_name='关键词，用于搜索')
+    keywords = models.CharField(max_length=100, null=True, blank=True, verbose_name='关键词，用于搜索')
     tags = models.ManyToManyField(PTag, related_name='pic_tag')
     url = models.URLField(verbose_name='图片链接')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     no_words = models.BooleanField(default=False, verbose_name="无字")
-    blur_hash = models.CharField(max_length=256)
+    blur_hash = models.CharField(max_length=256, null=True, blank=True)
     mode = models.ForeignKey(Mode, on_delete=models.CASCADE, related_name='pics')
     
     class Meta:
@@ -72,7 +72,7 @@ class Pic(models.Model):
 class Topic(models.Model):
     mode = models.ForeignKey(Mode, on_delete=models.CASCADE, related_name='topics')
     title = models.CharField(max_length=100)
-    keywords = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=100, null=True, blank=True, help_text='用于搜索')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, related_name='topic_tag')
     pics = models.ManyToManyField(Pic, related_name='topics')
